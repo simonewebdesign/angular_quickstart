62f93ed9c16e26e2bf6e3443253742c11747f1f2
Simone Vittori <hello@simonewebdesign.it>
2015-03-12 16:51:31 +0000

    Component with separate template

--- myappcomponent.js
+++ myappcomponent.js
@@ -1,22 +1,17 @@
 import {Component, Foreach, Template, If} from 'angular2/angular2';
 
-console.log('FORM MUY APP COMPOENTN')
-// import {CalendarCell} from 'components/calendar-cell/calendar-cell';
-// import {searchAllCells} from 'stores/registry';
-// import _ from '../../node_modules/lodash/index';
+// Annotation section
+@Component({
+  selector: 'my-app-component'
+})
 
-// // Annotation section
-// // @Component({
-// //   selector: 'my-app'
-// // })
+@Template({
+  url: System.baseURL+'src/components/my-app-component/my-app-component.html'
+})
 
-// // @Template({
-// //   inline: '<h1>Hello {{ name }}</h1>'
-// // })
-
-// // Component controller
-// export class MyAppComponent {
-//   constructor() {
-//     this.name = 'Alice';
-//   }
-// }
+// Component controller
+export class MyAppComponent {
+  constructor() {
+    this.name = 'Alice';
+  }
+}
--- src/components/foo.es6
+++ src/components/foo.es6
@@ -1,5 +1,4 @@
 export default class Foo {
-  
   foo() {
     return "bar";
   }
