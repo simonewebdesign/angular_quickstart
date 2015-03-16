9266245956042de6d82615a898b322f6740aaf38
Simone Vittori <hello@simonewebdesign.it>
2015-03-16 12:42:12 +0000

    Improve websocket server

--- ws-server.js
+++ ws-server.js
@@ -2,15 +2,18 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 var jsonFile = require('./medications.json');
+var now = function () {
+  return '[' + new Date().toUTCString() + '] ';
+}
 
 var server = http.createServer(function(request, response) {
-  console.log('Received request from ' + request.url);
+  console.log(now()+'Received request from ' + request.url);
   response.writeHead(404);
   response.end();
 });
 
 server.listen(1337, function() {
-    console.log('Server is listening on port 1337.');
+    console.log(now()+'Server is listening on port 1337.');
 });
 
 wsServer = new WebSocketServer({
@@ -21,16 +24,16 @@ wsServer = new WebSocketServer({
 function isAllowedOrigin(origin) {
   valid_origins = ['http://localhost:8080'];
   if (valid_origins.indexOf(origin) != -1) {
-    console.log('Connection accepted from origin ' + origin);
+    console.log(now()+'Connection accepted from origin ' + origin);
     return true;
   } else {
-    console.log('Connection rejected from origin ' + origin);
+    console.log(now()+'Connection rejected from origin ' + origin);
     return false;
   }
 }
 
 wsServer.on('connection', function(webSocketConnection) {
-  console.log('Connection started.');
+  console.log(now()+'Connection started.');
 });
 
 wsServer.on('request', function(request) {
@@ -39,13 +42,13 @@ wsServer.on('request', function(request) {
     request.reject();
 
   connection.on('message', function(message) {
-    console.log('Received Message: ' + message.utf8Data);
+    console.log(now()+'Received Message: ' + message.utf8Data);
 
     if (message.type === 'utf8' && message.utf8Data === 'medications') {
       connection.sendUTF(JSON.stringify(jsonFile));
     }
   });
   connection.on('close', function(reasonCode, description) {
-      console.log(connection.remoteAddress + ' has been disconnected.');
+      console.log(now()+'Connection closed (reason: '+reasonCode+'). '+description+'.');
   });
 });
